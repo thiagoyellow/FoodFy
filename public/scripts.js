d4426c24514a32581b17981bfd4e79447edbf317
thiagoyellow <Administrador@COOPANABB.LOCAL>
2021-02-18 17:09:12 -0300

    chore: recipe page adicionada

--- public/scripts.js
+++ public/scripts.js
@@ -6,8 +6,14 @@ for (let card of cards) {
     card.addEventListener("click", function() {
         const recipeId = card.getAttribute("id")
         window.location.href = `/recipe?id=${recipeId}`
-
     })
 }
 
+function Mudarestado(el) {
+    const display = document.getElementById(el).style.display;
+    if (display == "none")
+      document.getElementById(el).style.display = 'block';
+    else
+      document.getElementById(el).style.display = 'none';
+  }
 
