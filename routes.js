473f5ea043fc21c77c3bedf9b076171e8bdf2778
thiagoyellow <thiago.g.santos@outlook.com.br>
2021-05-31 07:54:37 -0300

    chore:init admin

---
+++ routes.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const routes = express.Router()
+
+const server = express()
+const recipes = require("./data")
+
+routes.get('/', function(req, res) {
+  return res.render("home", {items: recipes})
+})
+
+routes.get('/about', function(req, res) {
+  return res.render("about")
+})
+
+routes.get('/recipe', function(req, res) {
+  const id = req.query.id
+  const recipe = recipes.find(function(recipe){
+      return recipe.id == id
+  })
+
+  if(!recipe) {
+      return res.send("Receita não encontrada!")
+  }
+
+  return res.render("recipe", { item: recipe })
+})
+
+
+routes.get('/recipes', function(req, res) {
+  return res.render("recipes")
+})
+
+
+routes.get('/admin', function(req, res) {
+  return res.render("admin/index")
+})
+
+routes.use(function (req, res) {
+  res.status(404).render("not-found");
+});
+
+module.exports = routes
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const routes = require("./routes")
 
 const server = express()
-const recipes = require("./data")
 
 server.use(express.static('public'))
+server.use(routes)
 
 server.set("view engine", "njk")
 
@@ -12,33 +13,6 @@ nunjucks.configure("views", {
     express:server,
     autoscape: false,
     noCache: true
-})
-
-server.get("/", function(req, res){
-    return res.render("home", {items: recipes})
-})
-
-server.get("/about", function(req, res) {
-    return res.render("about")
-})
-
-server.get("/recipes", function(req, res) {
-    return res.render("recipes", {items: recipes})
-})
-
-server.get("/recipe", function(req, res) {
-    const id = req.query.id
-    
-    const recipe = recipes.find(function(recipe){
-        return recipe.id == id
-    })
-
-    if(!recipe) {
-        return res.send("Receita não encontrada!")
-    }
-
-    return res.render("recipe", { item: recipe })
-
 })
 
 server.listen(5000, function() {
