d4b64980f4f9f4244e0c5515b802e4c5696d9f51
Thiago Gomes <thiago.g.santos@outlook.com.br>
2021-06-16 16:55:53 -0300

    refazendo estilos

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,10 @@ const express = require('express')
 const routes = express.Router()
 
 const server = express()
-const recipes = require("./data")
+const recipes = require("../data")
 
 routes.get('/', function(req, res) {
-  return res.render("home", {items: recipes})
+  return res.render("index", {items: recipes})
 })
 
 routes.get('/about', function(req, res) {
@@ -16,7 +16,7 @@ routes.get('/recipe', function(req, res) {
   const id = req.query.id
   const recipe = recipes.find(function(recipe){
       return recipe.id == id
-  })
+})
 
   if(!recipe) {
       return res.send("Receita não encontrada!")
@@ -25,11 +25,9 @@ routes.get('/recipe', function(req, res) {
   return res.render("recipe", { item: recipe })
 })
 
-
 routes.get('/recipes', function(req, res) {
   return res.render("recipes")
 })
-
 
 routes.get('/admin', function(req, res) {
   return res.render("admin/index")
