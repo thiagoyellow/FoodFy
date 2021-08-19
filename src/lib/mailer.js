0c9c4406abe9aba914d882fce4c2fbfa5ff0d141
thiagoyellow <thiago.g.santos@outlook.com.br>
2021-08-19 17:13:10 -0300

    fix: problema no nodemailer corrigido

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -4,8 +4,7 @@ module.exports = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
     port: 2525,
     auth: {
-      user: "cdcba590b24f78",
-      pass: "4668b5eade20f5"
+      user: "f91e63a309ce1f",
+      pass: "43836a7e0f4451"
     }
-  });
-
+});
